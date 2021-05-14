9396a39729ec8953fd206ab73d417819afd6b3db
Jeff Ripke <jripke74@gmail.com>
2021-05-14 09:57:29 -0700

    04 add logic for modal

--- shared.js
+++ shared.js
@@ -1,3 +1,10 @@
 var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButtons = document.querySelectorAll('.plan button');
 
-backdrop.style.display = 'block';
+for (var i = 0; i < selectPlanButtons.length; i++) {
+  selectPlanButtons[i].addEventListener('click', () => {
+    modal.style.display = 'block';
+    backdrop.style.display = 'block';
+  });
+}
