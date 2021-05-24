3dbb1481e7ef21d17f92cddb3aedccec758fbf8a
Jeff Ripke <jripke74@gmail.com>
2021-05-24 11:17:15 -0700

    09 add classList

--- shared.js
+++ shared.js
@@ -6,25 +6,25 @@ const toggleButton = document.querySelector('.toggle-button');
 const mobileNav = document.querySelector('.mobile-nav');
 
 const closeModal = () => {
-  backdrop.style.display = 'none';
-  modal.style.display = 'none';
+  modal.classList.remove('open');
+  backdrop.classList.remove('open');
 }
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener('click', () => {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 }
 
 backdrop.addEventListener('click', () => {
-  mobileNav.style.display = 'none';
+  mobileNav.classList.remove('open');
   closeModal();
 });
 
 modalNoButton.addEventListener('click', closeModal);
 
 toggleButton.addEventListener('click', () => {
-  mobileNav.style.display = 'block';
-  backdrop.style.display = 'block';
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 });
